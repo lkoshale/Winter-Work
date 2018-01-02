84d2140ac58666f3b0d7aa69cdb66690be03d382
lkoshale <lkoshale@gmail.com>
2018-01-02 19:28:29 +0530

    email classifier completed

--- courseraML assgn/machine-learning-ex6/ex6/dataset3Params.m
+++ courseraML assgn/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+arry = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+
+% error = ones(64,1);
+% k = 1;
+% for i =1:8
+%     ci = arry(i);
+%     for j = 1:8
+%         si = arry(j);
+%         model= svmTrain(X, y, ci, @(x1, x2) gaussianKernel(x1, x2, si));
+%         pred = svmPredict(model,Xval);
+%         disp(size(pred));
+%         disp(size(yval));
+%         error(k) = mean(double(ne(pred,yval)));
+%         k=k+1;
+%     end
+% end
+% [M,I]= min(error);
+% disp([M,I]);
+% disp(error);
+% 
+% 
+% model= svmTrain(X, y, 1, @(x1, x2) gaussianKernel(x1, x2, 0.1));
+% pred = svmPredict(model,Xval);
+% e1 = mean(double(ne(pred,yval)))
+
+C = 1;
+sigma = 0.1;
+
 
 
 
--- courseraML assgn/machine-learning-ex6/ex6/processEmail.m
+++ courseraML assgn/machine-learning-ex6/ex6/processEmail.m
@@ -97,7 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
+    for w=1:length(vocabList)
+        if strcmp(str,vocabList{w})==1
+            word_indices = [word_indices ; w];
+        end
+            
+    end
 
 
 
